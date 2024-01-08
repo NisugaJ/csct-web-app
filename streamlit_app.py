4d21fc319d7f8a703480c0846a89861729d464bd
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2024-01-08 22:55:47 +0000

    double grey

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Home", page_icon='🌱', layout='wide')
 
 sidebar = st.sidebar
 sidebar.title(':green[Planta]', anchor=False)
-sidebar.write(':grey[Affordable plant-based food better than meat]')
+sidebar.write(':grey[:grey[Affordable plant-based food better than meat]]')
 
 
 st.write('### Welcome to Planta!')
