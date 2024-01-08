4a0a5803d3295b0c9c7e20e36c5e849a77367437
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2024-01-08 22:51:48 +0000

    balanced cols

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ sidebar.write(':grey[Affordable plant based food better than meat]')
 
 
 st.write('### Welcome to Planta!')
-col1, col2, col3 = st.columns(3)
+st.divider()
+left_col2, left_col1, col1, col2, col3, right_col1, right_col2 = st.columns([1, 1, 3, 3, 3, 1, 1])
 
 with col1:
    st.link_button(
