19df3b2f16cf0b1d770efde21bc7a15eb63e3348
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2024-01-08 20:07:36 +0000

    all products page

--- pages/AllProducts.py
+++ pages/AllProducts.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 import requests
+from st_aggrid import AgGrid
+
+from streamlit_utils import filter_dataframe
 
 api_url = st.secrets["API_BASE_URL"]
 
@@ -7,5 +11,11 @@ all_products = requests.get(f"{api_url}/products/all").json()
 
 st.info(f"{len(all_products)} products are available")
 
-all_products
 
+df = pd.DataFrame(
+    all_products,
+)
+
+filter_dataframe(df)
+
+ag_grid = AgGrid(df,  allow_unaligned_rows=True, theme='streamlit', height=400, width='100%', enable_quicksearch=True)
