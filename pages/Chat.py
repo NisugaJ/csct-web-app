4c2d87e5b5e9077ce2466e1eb98261f7435957a8
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2024-01-09 03:50:06 +0000

    full_response

--- pages/Chat.py
+++ pages/Chat.py
@@ -54,16 +54,12 @@ with st.chat_message("assistant"):
                 "Do you need help?", ]
         )
 
-        add_on = """I'm PlantaAI Chat Bot. I can help you with plant-based and meat/dairy products. 
-            I can help with below scenarios:
-             
-            1. Compare a given 
-            plant-based product and a given meat/dairy product based on prices, weight, ingredients, nutrition's, 
-            and much more.
-            
-            2. Select the best plant-based product based on your preferences.
-            
-            """
+        add_on = "I'm PlantaAI Chat Bot. I can help you with plant-based and meat/dairy products. \
+            I can help with below scenarios: \
+            1. Compare a given \
+            plant-based product and a given meat/dairy product based on prices, weight, ingredients, nutrition's, \
+            and much more. \
+            2. Select the best plant-based product based on your preferences."
 
         full_response = assistant_response + add_on
 
